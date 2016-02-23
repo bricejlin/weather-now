0d8441a98aec5bc09da19031dfb0e92a21e3e3b5
Brice Lin <bricejlin@gmail.com>
2016-02-23 08:59:08 -0500

    Add initial test

---
+++ test/reducers.spec.js
@@ -0,0 +1,13 @@
+import { expect } from 'chai';
+
+import weather from '../src/reducers/weather';
+
+describe('REDUCERS', () => {
+  describe('Weather Reducer', () => {
+    it('should return default state if action is undefined', () => {
+      const initialState = {};
+      const nextState = weather(initialState, 'BLAH');
+      expect(nextState).to.deep.equal(initialState);
+    });
+  });
+});
