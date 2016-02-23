5d60d75a99bbdb34d9e586cc2f7daee758e8e2df
Brice Lin <bricejlin@gmail.com>
2016-02-22 22:28:50 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# Real-time weather reporting for your device's exact location / time
+
+## Coding Practices
+
+### Tech that must be used
+- React
+- Redux
+- Webpack
+- Babel
+- React Router
+
+### Every PR must be accompanied by a test
---
+++ src/containers/Root.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+export default class Root extends Component {
+  render() {
+    return (
+      <div>
+        <h1>weatherNOW</h1>
+        <p>Your #1 source for real-time, one place, one time, one location weather data</p>
+      </div>
+    );
+  }
+}
