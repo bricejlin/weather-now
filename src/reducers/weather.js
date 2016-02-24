9813d87eea9ed787649d415fe0abaedd02cf2519
Brice Lin <bricejlin@gmail.com>
2016-02-23 23:14:19 -0500

    Add more tests for weather reducer

--- src/reducers/weather.js
+++ src/reducers/weather.js
@@ -1,9 +1,10 @@
 import { RECEIVE_WEATHER_DATA } from '../constants';
+import objectAssign from 'object-assign';
 
 export default function now(state = {}, action) {
   switch (action.type) {
   case RECEIVE_WEATHER_DATA:
-    return Object.assign({}, state, action.data);
+    return objectAssign({}, state, action.data);
   default:
     return state;
   }
--- test/reducers.spec.js
+++ test/reducers.spec.js
@@ -1,13 +1,28 @@
 import { expect } from 'chai';
 
+import * as types from '../src/constants';
 import weather from '../src/reducers/weather';
 
 describe('REDUCERS', () => {
   describe('Weather Reducer', () => {
     it('should return default state if action is undefined', () => {
       const initialState = {};
-      const nextState = weather(initialState, 'BLAH');
+      const nextState = weather(undefined, 'BLAH');
       expect(nextState).to.eql(initialState);
+    });
+
+    it('should return new state from a RECEIVE_WEATHER_DATA action', () => {
+      const action = {
+        type: types.RECEIVE_WEATHER_DATA,
+        data: {
+          main: {
+            temp: 39
+          }
+        }
+      };
+
+      const nextState = weather({}, action);
+      expect(nextState.main.temp).to.equal(39);
     });
   });
 });
