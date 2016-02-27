d1e80663076c180e02344af263acf6e5ab10d50b
Brice Lin <bricejlin@gmail.com>
2016-02-27 16:31:11 -0500

    Use geolocatoin api to get users location coordinates

--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
 import 'babel-polyfill';
 import React from 'react';
-import { render } from 'react-dom'
+import { render } from 'react-dom';
 import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
-import logger from 'redux-logger'
-import thunk from 'redux-thunk'
-import rootReducer from './reducers'
-import { getWeatherData } from './actions'
+import logger from 'redux-logger';
+import thunk from 'redux-thunk';
+import rootReducer from './reducers';
+import { getWeatherData } from './actions';
 import Root from './containers/Root';
+import debugCreator from 'debug';
+
+const debug = debugCreator('geolocation');
+
 
 const middleware = process.env.NODE_ENV === 'production' ?
   [ thunk ] :
@@ -18,11 +22,31 @@ const store = createStore(
   applyMiddleware(...middleware)
 );
 
-// New York
-store.dispatch(getWeatherData({
-  lat: '40.7127840',
-  lon: '-74.0059410'
-}));
+function success(pos) {
+  const lat = pos.coords.latitude;
+  const lon = pos.coords.longitude;
+
+  debug('Successfully retrieved location', [lat, lon]);
+
+  store.dispatch(getWeatherData({ lat, lon }));
+}
+
+function error() {
+  console.log('Unable retrieve user location. Defaulting to NY');
+
+  debug('Failed to retrieve location');
+
+  store.dispatch(getWeatherData({
+    lat: '40.7127840',
+    lon: '-74.0059410'
+  }));
+}
+
+if (navigator.geolocation) {
+  navigator.geolocation.getCurrentPosition(success, error);
+}
+
+window.myDebug = debugCreator;
 
 render(
   <Provider store={store}>
