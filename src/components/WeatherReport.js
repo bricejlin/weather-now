99b963b19c4397480d33c0d2b423385d4d355542
Brice Lin <bricejlin@gmail.com>
2016-03-23 23:36:18 -0400

    Change css layout

--- src/components/WeatherReport.js
+++ src/components/WeatherReport.js
@@ -1,14 +1,20 @@
 import React, { Component, PropTypes } from 'react';
 
 export default class WeatherReport extends Component {
+
   render() {
     const { data, statusText } = this.props;
 
     return (
       <div>
         {statusText ? <div>{statusText}</div> : null}
-        <div>
-          <h2>New York {data.currently.temperature} °F</h2>
+        <div className="degrees-container">
+          <span className="logo">weatherNOW</span>
+          <h1>
+            <span>{ data.timezone ? data.timezone.split('/')[1].replace('_', ' ') : ''} </span>
+            <span className="number">{data.currently.temperature}</span>
+            <span> °F</span>
+          </h1>
         </div>
       </div>
     );
